e8e1fc5287e8c97492da6c14f678e34d3b20de9e
ice <ice@icelord.net>
2017-09-20 22:54:16 +0300

    outside temp added and indiglo controll

--- src/odometer.ino
+++ src/odometer.ino
@@ -23,8 +23,8 @@
    // LED Clk <- • 6      A3 •
    //  LED En <- • 7   N  A2 •
    // LED RST -> • 8   A  A1 •
-   //  Needle <- • 9   N  A0 •
-   //  DIMING -> • 10  O Arf •
+   //  Needle <- • 9   N  A0 • <- DIMMING
+   // Indiglo <- • 10  O Arf •
    //   Setup -> • 11    3V3 •
    //  Button -> • 12 ||| 13 • <- OneWire DS1820
    //            +-----------+
@@ -33,26 +33,28 @@
 // TODO
 //   outside temp
 //   racelogic bum edition )))
+//   longterm l/100km
+//   instant l/100km
 
 #define DEBUG
 
 #include <avr/pgmspace.h>
 #include <Arduino.h>
 #include <font5x7.h> // Local font lib
-#include <LedDisplay.h>
-#include <I2C_eeprom.h>
+#include <LedDisplay.h> //  platformio lib install "LedDisplay"
+#include <I2C_eeprom.h> // platformio lib install "I2C_EEPROM"
 #include <avr/wdt.h>
-#include <OneWire.h>
+#include <OneWire.h> // platformio lib install "OneWire"
 
 OneWire ds(13);                   // DS1820 Temperature sensor
 I2C_eeprom eeprom(0x50,16384/8);  // FM24C16A FRAM
 
 #define PPR 4 // VSS pulses per axle revolution
 
-#define TRIP_A        0
-#define TRIP_B        1
-#define MOTOR_HOUR    2
-#define OUTSIDE_TEMP  3
+#define TRIP_A              0
+#define TRIP_B              1
+#define MOTOR_HOUR          2
+#define OUTSIDE_TEMP        3
 
 #define MAX_SHOW      3
 
@@ -65,22 +67,30 @@ uint16_t TIRE_WIDTH_ARRAY[] = { 165, 175, 185, 195, 205, 215, 225, 235, 245, 255
 uint8_t TIRE_SIDE_ARRAY[] =   { 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85 };
 uint8_t TIRE_RIM_ARRAY[] =    { 13, 14, 15, 16, 17, 18, 19, 20, 21, 22 };
 
-#define CONFIG_POS_MAX 9
+#define CONFIG_POS_MAX 11
 
 // CONFIG
 uint8_t TIRE_RIM;
 uint8_t TIRE_WIDTH;
 uint8_t TIRE_SIDE;
 #define NEEDLE_STEP 16
-uint8_t NEEDLE_DIMMED;
-uint8_t NEEDLE_UNDIMMED;
 #define NEEDLE_DIMMED_DEFAULT     128
 #define NEEDLE_UNDIMMED_DEFAULT   255
+uint8_t NEEDLE_DIMMED;
+uint8_t NEEDLE_UNDIMMED;
+
 #define MAX_DISPLAY 15
-uint8_t DISPLAY_DIMMED;
-uint8_t DISPLAY_UNDIMMED;
 #define DISPLAY_DIMMED_DEFAULT    10
 #define DISPLAY_UNDIMMED_DEFAULT  15
+uint8_t DISPLAY_DIMMED;
+uint8_t DISPLAY_UNDIMMED;
+
+#define INDIGLO_STEP 16
+#define INDIGLO_DIMMED_DEFAULT    128
+#define INDIGLO_UNDIMMED_DEFAULT  255
+uint8_t INDIGLO_DIMMED;
+uint8_t INDIGLO_UNDIMMED;
+
 double TOTAL_TRIP;
 double DAILY_TRIP_A;
 double DAILY_TRIP_B;
@@ -112,8 +122,9 @@ uint8_t DIMMING;
 // PINS CONFIG
 #define BUTTON 12
 #define NEEDLE 9
-#define DIMPIN 10
+#define DIMPIN A0
 #define SETUP_PIN 11
+#define INDIGLO 10
 
 #define LED_dataPin 4
 #define LED_registerSelect 5
@@ -151,6 +162,7 @@ uint8_t type_s;
 uint8_t DISPLAY_MODE=DISPLAY_TRIP;
 uint8_t DEFAULT_BRIGHTNESS;
 uint8_t DEFAULT_NEEDLE;
+uint8_t DEFAULT_INDIGLO;
 
 void CalcTire()
 {
@@ -193,6 +205,7 @@ void setup()
 #endif
         pinMode(DIMPIN, INPUT);
         pinMode(NEEDLE, OUTPUT);
+        pinMode(INDIGLO, OUTPUT);
         pinMode(BUTTON, INPUT_PULLUP);
         pinMode(SETUP_PIN, INPUT_PULLUP);
         pinMode(VSS_PIN, INPUT_PULLUP);
@@ -229,6 +242,8 @@ void setup()
         eeprom.readBlock(28,(uint8_t*) &NOMINAL_RPM, 4);
         eeprom.readBlock(32,(uint8_t*) &MOTOR_HOURS_LIMIT, 2);
         eeprom.readBlock(34,(uint8_t*) &LEADING_ZERO, 1);
+        eeprom.readBlock(35,(uint8_t*) &INDIGLO_DIMMED, 1);
+        eeprom.readBlock(36,(uint8_t*) &INDIGLO_UNDIMMED, 1);
 #ifdef DEBUG
         Serial.print("EEPROM TOTAL: "); Serial.print(TOTAL_TRIP);
         Serial.print(", A: "); Serial.print(DAILY_TRIP_A);
@@ -244,6 +259,9 @@ void setup()
         Serial.print("EEPROM DISPLAY: Day:"); Serial.print(DISPLAY_UNDIMMED);
         Serial.print("Night:"); Serial.print(DISPLAY_DIMMED);
         Serial.print('\n');
+        Serial.print("EEPROM INDIGLO: Day:"); Serial.print(INDIGLO_UNDIMMED);
+        Serial.print("Night:"); Serial.print(INDIGLO_DIMMED);
+        Serial.print('\n');
         Serial.print("EEPROM MOTOR: Hours"); Serial.print(MOTOR_HOURS);
         Serial.print("RPM:"); Serial.print(NOMINAL_RPM);
         Serial.print("Limit:"); Serial.print(MOTOR_HOURS_LIMIT);
@@ -257,6 +275,8 @@ void setup()
         if (NEEDLE_UNDIMMED<1) NEEDLE_UNDIMMED=NEEDLE_UNDIMMED_DEFAULT;
         if (DISPLAY_DIMMED<1) DISPLAY_DIMMED=DISPLAY_DIMMED_DEFAULT;
         if (DISPLAY_UNDIMMED<1) DISPLAY_UNDIMMED=DISPLAY_UNDIMMED_DEFAULT;
+        if (INDIGLO_DIMMED<1) INDIGLO_DIMMED=INDIGLO_DIMMED_DEFAULT;
+        if (INDIGLO_UNDIMMED<1) INDIGLO_UNDIMMED=INDIGLO_UNDIMMED_DEFAULT;
         if (NOMINAL_RPM<500) NOMINAL_RPM=2000;
         if (MOTOR_HOURS_LIMIT<100) MOTOR_HOURS_LIMIT=DEFAULT_MOTOR_HOURS_LIMIT;
         LEADING_ZERO=LEADING_ZERO & 1;
@@ -274,7 +294,7 @@ void setup()
         }
         DEFAULT_BRIGHTNESS=DISPLAY_UNDIMMED;
         DEFAULT_NEEDLE=NEEDLE_UNDIMMED;
-
+        DEFAULT_INDIGLO=INDIGLO_UNDIMMED;
         wdt_reset();
         delay(LOGO_DELAY);
         wdt_reset();
@@ -353,16 +373,20 @@ void loop()
                 if (!DIMMED) { // JUST DIMMED and fade in
                         if (DEFAULT_BRIGHTNESS>DISPLAY_DIMMED) DEFAULT_BRIGHTNESS--;
                         if (DEFAULT_NEEDLE>NEEDLE_DIMMED) DEFAULT_NEEDLE-=NEEDLE_STEP;
-                        if ((DEFAULT_BRIGHTNESS==DISPLAY_DIMMED) && (DEFAULT_NEEDLE<=NEEDLE_DIMMED)) DIMMED=true;
+                        if (DEFAULT_INDIGLO>INDIGLO_DIMMED) DEFAULT_INDIGLO-=INDIGLO_STEP;
+                        if ((DEFAULT_BRIGHTNESS==DISPLAY_DIMMED) && (DEFAULT_NEEDLE<=NEEDLE_DIMMED) && (DEFAULT_INDIGLO<=INDIGLO_DIMMED)) DIMMED=true;
                         setBrightness(DEFAULT_BRIGHTNESS);
                         analogWrite(NEEDLE, DEFAULT_NEEDLE);
+                        analogWrite(INDIGLO, DEFAULT_INDIGLO);
                 } else {
                         if (DIMMED) { // JUST UNDIMMED and fade out
                                 if (DEFAULT_BRIGHTNESS<DISPLAY_UNDIMMED) DEFAULT_BRIGHTNESS++;
                                 if (DEFAULT_NEEDLE<NEEDLE_UNDIMMED) DEFAULT_NEEDLE+=NEEDLE_STEP;
-                                if ((DEFAULT_BRIGHTNESS>=DISPLAY_UNDIMMED) && (DEFAULT_NEEDLE>=NEEDLE_UNDIMMED)) DIMMED=false;
+                                if (DEFAULT_INDIGLO<INDIGLO_UNDIMMED) DEFAULT_INDIGLO+=INDIGLO_STEP;
+                                if ((DEFAULT_BRIGHTNESS>=DISPLAY_UNDIMMED) && (DEFAULT_NEEDLE>=NEEDLE_UNDIMMED) && (DEFAULT_INDIGLO>=INDIGLO_UNDIMMED)) DIMMED=false;
                                 setBrightness(DEFAULT_BRIGHTNESS);
                                 analogWrite(NEEDLE, DEFAULT_NEEDLE);
+                                analogWrite(INDIGLO, DEFAULT_INDIGLO);
                         }
                 }
         }
@@ -393,6 +417,8 @@ void loop()
                                         eeprom.writeBlock(28,(uint8_t*) &NOMINAL_RPM, 4);
                                         eeprom.writeBlock(32,(uint8_t*) &MOTOR_HOURS_LIMIT, 2);
                                         eeprom.writeBlock(34,(uint8_t*) &LEADING_ZERO, 1);
+                                        eeprom.writeBlock(35,(uint8_t*) &INDIGLO_DIMMED, 1);
+                                        eeprom.writeBlock(36,(uint8_t*) &INDIGLO_UNDIMMED, 1);
                                         DISPLAY_MODE=DISPLAY_TRIP;
                                         SETUP_DO=false;
                                         CalcTire(); // Recalc circumference size
@@ -448,6 +474,7 @@ void loop()
                         LIMIT_BLINK=true;
                         setBrightness(DISPLAY_UNDIMMED_DEFAULT);
                         analogWrite(NEEDLE,NEEDLE_UNDIMMED_DEFAULT);
+                        analogWrite(INDIGLO,INDIGLO_UNDIMMED_DEFAULT);
                 } else  // Show total trip
                 {
                         if (LEADING_ZERO)
@@ -462,9 +489,11 @@ void loop()
                                 {
                                         setBrightness(DISPLAY_DIMMED);
                                         analogWrite(NEEDLE, NEEDLE_DIMMED);
+                                        analogWrite(INDIGLO, INDIGLO_DIMMED);
                                 } else {
                                         setBrightness(DISPLAY_UNDIMMED);
                                         analogWrite(NEEDLE, NEEDLE_UNDIMMED);
+                                        analogWrite(INDIGLO, INDIGLO_UNDIMMED);
                                 }
                         }
                         LIMIT_BLINK=false;
@@ -511,11 +540,15 @@ void loop()
                         break;
                 case 6: sprintf(buffer," DisplayNight% 3d ",DISPLAY_DIMMED);
                         break;
-                case 7: sprintf(buffer," NominalRPM% 5d ",NOMINAL_RPM);
+                case 7: sprintf(buffer," INDIGLODay% 5d ",INDIGLO_UNDIMMED);
                         break;
-                case 8: sprintf(buffer,"MotrHourLmt% 4d ",MOTOR_HOURS_LIMIT);
+                case 8: sprintf(buffer," INDIGLONight% 3d ",INDIGLO_DIMMED);
                         break;
-                case 9: sprintf(buffer," LeadingZero=%d  ",LEADING_ZERO);
+                case 9: sprintf(buffer," NominalRPM% 5d ",NOMINAL_RPM);
+                        break;
+                case 10: sprintf(buffer,"MotrHourLmt% 4d ",MOTOR_HOURS_LIMIT);
+                        break;
+                case 11: sprintf(buffer," LeadingZero=%d  ",LEADING_ZERO);
                         break;
                 }
 
@@ -570,14 +603,22 @@ void loop()
                                                 setBrightness(DISPLAY_DIMMED);
                                                 break;
                                         case 7:
+                                                INDIGLO_UNDIMMED=+INDIGLO_STEP;
+                                                analogWrite(INDIGLO,INDIGLO_UNDIMMED);
+                                                break;
+                                        case 8:
+                                                INDIGLO_DIMMED=+INDIGLO_STEP;
+                                                analogWrite(INDIGLO,INDIGLO_DIMMED);
+                                                break;
+                                        case 9:
                                                 NOMINAL_RPM+=NOMINAL_STEP;
                                                 if (NOMINAL_RPM>MAX_NOMINAL) NOMINAL_RPM=NOMINAL_STEP;
                                                 break;
-                                        case 8:
+                                        case 10:
                                                 MOTOR_HOURS_LIMIT+=MOTOR_HOURS_STEP;
                                                 if (MOTOR_HOURS_LIMIT>MAX_MOTOR_HOURS) MOTOR_HOURS_LIMIT=MOTOR_HOURS_STEP;
                                                 break;
-                                        case 9:
+                                        case 11:
                                                 LEADING_ZERO=!LEADING_ZERO;
                                                 break;
                                         }
